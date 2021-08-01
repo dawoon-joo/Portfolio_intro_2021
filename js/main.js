33ea6a7cd9d8dd55812d2113770af2bf9f6425c1
dawoon-joo <wnekdns5015@gamil.con>
2021-08-02 05:32:04 +0900

    스와이퍼 레이아웃 디자인 수정

--- js/main.js
+++ js/main.js
@@ -1,23 +1,49 @@
-const swiper = new Swiper(".mySwiper", {
-    spaceBetween: 0,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-});
-const swiper2 = new Swiper(".mySwiper2", {
-    loop:true,
+const menu = ["Company", "About", "Information", "News"];
+
+const swiper = new Swiper("#wrap", {
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 100,
     mousewheel: true,
+
     navigation: {
         nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        prevEl: ".swiper-button-prev"
     },
-    thumbs: {
-        swiper: swiper,
-    },
+
     pagination: {
         el: ".swiper-pagination",
         type: "bullets",
         clickable: true,
+        renderBullet: function(index, className){
+            return `<span class="${className}">${menu[index]}</span>`
+        }
+
     },
 });
+
+const bgs = document.querySelectorAll(".bg li");
+const navi = document.querySelectorAll(".swiper-pagination span");
+
+window.addEventListener("mousewheel", activation);
+swiper.on("slideChangeTransitionEnd", activation);
+
+for(let el of navi){
+    el.addEventListener("click", e=>{
+        const isOn = e.currentTarget.classList.contains("swiper-pagination-bullet-active");
+        if(isOn) return;
+        swiper.on("slideChangeTransitionEnd", activation);
+    })
+}
+
+
+function activation(){
+    let item = document.querySelector(".swiper-slide-active");
+    let i = item.getAttribute("data-swiper-slide-index");
+
+    for(let el of bgs){
+        el.classList.remove("on");
+    }
+    bgs[i].classList.add("on");
+}
